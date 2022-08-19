351cd21ade0dd61cfbae63baa0bfcd7ddfc8f715
Your Name <goncharovigor12@gmail.com>
2022-08-19 07:56:47 +0300

    calc

---
+++ math-helper/script.js
@@ -0,0 +1,8 @@
+let i1 = document.getElementById("i1");
+let i2 = document.getElementById("i2");
+let btn = document.getElementById("but_res");
+
+function getRes() {
+  const result = i1.value + i2.value;
+  console.log(result);
+}
